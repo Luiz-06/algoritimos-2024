741679ae465bacf39315e7cdf12e0209719f115b
Luiz.06 <160617306+Luiz-06@users.noreply.github.com>
2024-02-20 12:15:20 -0300

    Delete atividade_9.js

--- atividade_9.js
+++
@@ -1,7 +0,0 @@
-import {question} from 'readline-sync'
-
-
-    const n1=Number(question('Escreva um numero: '))
-    const n2=Number(question('Escreva um segundo numero: '))
-
-    console.log(`${n2} ${n1}`)
